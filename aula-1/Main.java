3cd0532cefb76883fb17e2285d830b759c2be642
blanqueandre <andrev2700@gmail.com>
2025-05-10 11:49:18 -0300

    Add files via upload

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 public class Main
 {
+     
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		
+        boolean ehVerdade = "andre" != "bruno";
+        
+        boolean chovendo = true;
+        boolean relampejando = true;
+        if (chovendo&&relampejando){
+            
+           System.out.println("nao sair de casa");
+           
+        }
+        
+        boolean macarrao = false;
+        boolean strogonofe = true;
+        boolean almoco = macarrao||strogonofe;
+        
+        
+        
 	}
 }
