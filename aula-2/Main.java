f89a084ae07384bb0ba83535a94a5c59644db6bd
blanqueandre <andrev2700@gmail.com>
2025-05-17 11:12:16 -0300

    Update Main.java

--- aula-2/Main.java
+++ aula-2/Main.java
@@ -2,6 +2,43 @@ public class Main
 {
 	public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
+		/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    
+	    double hamburguer = 7.50;
+	    int refri = 4;
+	    double batatas = 10.25;
+	    float nuggets = 11.25;
+	    
+	    System.out.println("O que você quer pedir?");
+		Scanner entrada = new Scanner(System.in);
+		String valorfinal = entrada.next();
+		
+		System.out.println("Quantos você quer?");
+		Scanner meio = new Scanner(System.in);
+		String quantidade = entrada.next();
+		int total = (3);
+		
+		
+		
+		
+			}
+}
+
 		System.out.println("Hello World");
 	}
 }
