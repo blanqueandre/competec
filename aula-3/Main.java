2b369eab56bc3a52cf98c523639239ba46a6fb3e
blanqueandre <andrev2700@gmail.com>
2025-05-24 11:25:46 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,7 +1,34 @@
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+        import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) { 
+	    
+	    Scanner entrada = new Scanner(System.in);
+		String palavra; 
+	    
+		int c = 0;
+		while(c < 5 ) {
+		    //System.out.println(  + " ");
+    		 palavra = entrada.next();
+    		 System.out.println(palavra + "\n");
+		     c++;
+		}
+		
+		import java.util.Scanner;
+public class Main
+{
+    
+	public static void main(String[] args) {
+	      Scanner tabuada10 = new Scanner(System.in);
+	   int tabuada;
+	   int numero = tabuada10.nextInt();
+	   for(int i = 1; i <= 10; i = i+1) { 
+	   tabuada = numero  * i;
+	    System.out.println(tabuada);
+	   }
 	}
 }
+	
